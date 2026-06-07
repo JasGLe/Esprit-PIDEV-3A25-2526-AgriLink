35ac23189a460261e373fa794c35a570533736bc
Jasser Yahyaoui <jasseryahyaoui36@gmail.com>
2026-06-07 20:26:04 +0100

    Chores: udpated repo's structure, enhanced readme file

--- public/assets/@symfony/stimulus-bundle/controllers-23db8bfd8215b11c0bbb7e5638708a3f.js
+++
@@ -1,7 +0,0 @@
-import controller_0 from "../ux-chartjs/controller.js";
-import controller_1 from "../ux-turbo/turbo_controller.js";
-import controller_2 from "../../controllers/hello_controller.js";
-import controller_3 from "../../controllers/poster_generator_controller.js";
-export const eagerControllers = {"symfony--ux-chartjs--chart": controller_0, "symfony--ux-turbo--turbo-core": controller_1, "hello": controller_2, "poster-generator": controller_3};
-export const lazyControllers = {"csrf-protection": () => import("../../controllers/csrf_protection_controller.js")};
-export const isApplicationDebug = true;
